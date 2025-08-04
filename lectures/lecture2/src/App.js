b2edd7766dd7ace0c5f88b1c28c33dd11335b9bf
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-04 15:50:23 +0900

    2.4 Animations and Pseudo Selectors

--- lectures/lecture2/src/App.js
+++ lectures/lecture2/src/App.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-    display: flex;
-`;
+// const Father = styled.div`
+//     display: flex;
+// `;
 
 // const Box = styled.div`
 //     background-color: ${(props) => props.bgColor};
@@ -21,25 +21,72 @@ const Father = styled.div`
 //     border-radius: 15px;
 // `;
 
-const Input = styled.input.attrs({
-    required: true,
-    minLength: 10
-})`
+// const Input = styled.input.attrs({
+//     required: true,
+//     minLength: 10
+// })`
+//     background-color: tomato;
+// `;
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`;
+
+const rotationAnimation = keyframes`
+    0% {
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+    50% {
+        border-radius: 100px;
+    }
+    100% {
+        transform: rotate(360deg);
+        border-radius: 0px;
+    }
+`;
+
+const Box = styled.div`
+    width: 200px;
+    height: 200px;
     background-color: tomato;
+    animation: ${rotationAnimation} 1s linear infinite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    span {
+        font-size: 36px;
+        &:hover{
+            font-size: 40px;
+        }
+        &:active {
+            opacity: 0;
+        }
+    };
 `;
 
 function App() {
   return (
-    <Father as="header">
-        {/* <Box bgColor="teal" />
-        <Circle bgColor="tomato" /> */}
-        {/* <Btn>Log in</Btn>
-        <Btn as="a" href="/">Log in</Btn> */}
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-    </Father>
+    // <Father as="header">
+    //     <Box bgColor="teal" />
+    //     <Circle bgColor="tomato" />
+    //     <Btn>Log in</Btn>
+    //     <Btn as="a" href="/">Log in</Btn>
+    //     <Input />
+    //     <Input />
+    //     <Input />
+    //     <Input />
+    // </Father>
+    <Wrapper>
+        <Box>
+            <span>😊</span>
+        </Box>
+    </Wrapper>
   );
 }
 
