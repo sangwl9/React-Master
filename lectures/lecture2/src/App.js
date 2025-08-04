8b5a197fba1787c2c3e2406987f41c9df1b3dde3
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-04 15:34:28 +0900

    2.3 'As' and Attrs

--- lectures/lecture2/src/App.js
+++ lectures/lecture2/src/App.js
@@ -4,21 +4,41 @@ const Father = styled.div`
     display: flex;
 `;
 
-const Box = styled.div`
-    background-color: ${(props) => props.bgColor};
-    width: 100px;
-    height: 100px;
-`;
+// const Box = styled.div`
+//     background-color: ${(props) => props.bgColor};
+//     width: 100px;
+//     height: 100px;
+// `;
 
-const Circle = styled(Box)`
-    border-radius: 50%;
+// const Circle = styled(Box)`
+//     border-radius: 50%;
+// `;
+
+// const Btn =  styled.button`
+//     color: white;
+//     background-color: tomato;
+//     border: 0;
+//     border-radius: 15px;
+// `;
+
+const Input = styled.input.attrs({
+    required: true,
+    minLength: 10
+})`
+    background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-        <Box bgColor="teal" />
-        <Circle bgColor="tomato" />
+    <Father as="header">
+        {/* <Box bgColor="teal" />
+        <Circle bgColor="tomato" /> */}
+        {/* <Btn>Log in</Btn>
+        <Btn as="a" href="/">Log in</Btn> */}
+        <Input />
+        <Input />
+        <Input />
+        <Input />
     </Father>
   );
 }
