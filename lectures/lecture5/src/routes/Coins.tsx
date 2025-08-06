b27d053fbd766a373a7d5123e114c290204d8c7a
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-06 21:03:42 +0900

    5.4 Route States

--- lectures/lecture5/src/routes/Coins.tsx
+++ lectures/lecture5/src/routes/Coins.tsx
@@ -25,9 +25,10 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom: 10px;
     a {
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
     }
     &:hover{
         a {
@@ -44,6 +45,12 @@ const Title = styled.h1`
 const Loader = styled.span`
     text-align: center;
     display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -75,7 +82,11 @@ function Coins() {
             <CoinsList>
                 {loading ? <Loader>Loading...</Loader> : coins.map((coin) => (
                     <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>
+                        <Link to={{
+                                pathname: `/${coin.id}`,
+                                state: { name: coin.name },
+                            }}>
+                            <Img src={`https://cryptoicon-api.pages.dev/api/icon/${coin.symbol.toLocaleLowerCase()}`}/>
                             {coin.name} &rarr;
                         </Link>
                     </Coin>
