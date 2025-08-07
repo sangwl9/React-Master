b707853abf170a83115acb22f118004d275a6500
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-07 13:38:05 +0900

    5.11 Recap

--- lectures/lecture5/src/routes/Coin.tsx
+++ lectures/lecture5/src/routes/Coin.tsx
@@ -143,18 +143,6 @@ function Coin(){
     const { state } = useLocation<RouteState>();
     const priceMatch = useRouteMatch("/:coinId/price");
     const chartMatch = useRouteMatch("/:coinId/chart");
-    // const [loading, setLoading] = useState(true);
-    // const [info, setInfo] = useState<InfoData>();
-    // const [priceInfo, setPriceInfo] = useState<PriceData>();
-    // useEffect(() => {
-    //     (async () => {
-    //         const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-    //         const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
-    //         setInfo(infoData);
-    //         setPriceInfo(priceData);
-    //         setLoading(false);
-    //     })()
-    // }, [coinId]);
     const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>({ queryKey: ["info", coinId], queryFn: () => fetchCoinInfo(coinId)});
     const { isLoading: tickersLoading, data: tikersData } = useQuery<PriceData>({ queryKey: ["tikers", coinId], queryFn: () => fetchCoinTickers(coinId)});
     const loading = infoLoading || tickersLoading;
