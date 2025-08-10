102a9a1e545bfa40739ee4681b787e8c8a5e2548
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-10 21:03:41 +0900

    6.5 To Do Setup

--- lectures/lecture6-coin/src/index.tsx
+++ lectures/lecture6-coin/src/index.tsx
@@ -1,11 +1,9 @@
 /**
  * npx create-react-app lecture3 --template typescript
  * npm i react@18.3.1 react-dom@18.3.1
- * npm i --save-dev @types/styled-components styled-components@5.3.10
- * npm i --save-dev @types/react-router-dom react-router-dom@5.3.4
- * npm i --save-dev @tanstack/react-query-devtools @tanstack/react-query
+ * npm i styled-components@5.3.10 react-router-dom@5.3.4 @tanstack/react-query react-helmet
+ * npm i --save-dev @types/styled-components @types/react-router-dom @tanstack/react-query-devtools @types/react-helmet
  * npm i --save react-apexcharts apexcharts
- * npm i --save-dev @types/react-helmet react-helmet
  * npm i recoil
  */
 
