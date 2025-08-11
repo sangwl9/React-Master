bdc0d194f5d1203ef0917cd734e5392571e92d5c
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-11 13:49:08 +0900

    6.7 Form Validation

--- lectures/lecture6-todo/src/ToDoList.tsx
+++ lectures/lecture6-todo/src/ToDoList.tsx
@@ -22,12 +22,16 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList (){
-    const { register, watch } = useForm();
-    console.log(watch());
+    const { register, handleSubmit, formState } = useForm();
+    const onValid = (data:any) => {
+        console.log(data);
+    }
+    console.log(formState.errors);
     return (
         <div>
-            <form>
-                <input {...register("toDo")} placeholder="Write a to do" />
+            <form onSubmit={handleSubmit(onValid)}>
+                <input {...register("email", {required: "email is required"})} placeholder="Email" />
+                <input {...register("username", {required: true, minLength: {value: 5, message: "too short"}})} placeholder="username" />
                 <button>Add</button>
             </form>
         </div>
