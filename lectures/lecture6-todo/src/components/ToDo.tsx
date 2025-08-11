9629039c7283812af72ac2fde0e28b81db37e605
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-11 15:59:54 +0900

    6.13 Categories

--- lectures/lecture6-todo/src/components/ToDo.tsx
+++ lectures/lecture6-todo/src/components/ToDo.tsx
@@ -1,12 +1,30 @@
+import { ButtonHTMLAttributes } from "react";
 import { IToDo } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+    };
     return (
         <li>
             <span>{text}</span>
-            <button>To Do</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    Done
+                </button>
+            )}
         </li>
     );
 }
