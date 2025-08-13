6fe21c30ce1b3e5e9039c870e9fad15d402a73ee
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-13 14:31:19 +0900

    7.1 Set Selectors

--- lectures/lecture7-trello/src/App.tsx
+++ lectures/lecture7-trello/src/App.tsx
@@ -67,9 +67,12 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
     const [minutes, setMinutes] = useRecoilState(minuteState);
-    const hours = useRecoilValue(hourSelector);
+    const [hours, setHours] = useRecoilState(hourSelector);
     const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
         setMinutes(+event.currentTarget.value);
+    };
+    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
     };
     return (
         <>
@@ -81,7 +84,12 @@ function App() {
                     type="number"
                     placeholder="Minutes"
                 />
-                <input value={hours} type="number" placeholder="Hours" />
+                <input
+                    value={hours}
+                    onChange={onHoursChange}
+                    type="number"
+                    placeholder="Hours"
+                />
             </div>
         </>
     );
