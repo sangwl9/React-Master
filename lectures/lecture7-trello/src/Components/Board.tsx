b82f3fa4b5783fffe0bd93142f26396731cb3bce
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-13 17:27:01 +0900

    7.11 Droppable Snapshot

--- lectures/lecture7-trello/src/Components/Board.tsx
+++ lectures/lecture7-trello/src/Components/Board.tsx
@@ -9,6 +9,8 @@ const Wrapper = styled.div`
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 300px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -16,6 +18,22 @@ const Title = styled.h2`
     font-weight: 600;
     margin-bottom: 10px;
     font-size: 18px;
+`;
+
+interface IAreaProps {
+    isDraggingOver: boolean;
+    isDraggingFromThis: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+    background-color: ${(props) =>
+        props.isDraggingOver
+            ? "pink"
+            : props.isDraggingFromThis
+            ? "red"
+            : "blue"};
+    flex-grow: 1;
+    transition: background-color 0.3s ease-in-out;
 `;
 
 interface IBoard {
@@ -28,8 +46,15 @@ function Board({ toDos, boardId }: IBoard) {
         <Wrapper>
             <Title>{boardId}</Title>
             <Droppable droppableId={boardId}>
-                {(magic) => (
-                    <div ref={magic.innerRef} {...magic.droppableProps}>
+                {(magic, snapshot) => (
+                    <Area
+                        isDraggingOver={snapshot.isDraggingOver}
+                        isDraggingFromThis={Boolean(
+                            snapshot.draggingFromThisWith,
+                        )}
+                        ref={magic.innerRef}
+                        {...magic.droppableProps}
+                    >
                         {toDos.map((toDo, index) => (
                             <DragabbleCard
                                 key={toDo}
@@ -38,7 +63,7 @@ function Board({ toDos, boardId }: IBoard) {
                             />
                         ))}
                         {magic.placeholder}
-                    </div>
+                    </Area>
                 )}
             </Droppable>
         </Wrapper>
