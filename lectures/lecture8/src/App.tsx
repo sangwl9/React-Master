69ac1e82d3c7b7c06014ae9867cfa9071e520002
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-14 15:44:50 +0900

    8.6 Gestures part Two

--- lectures/lecture8/src/App.tsx
+++ lectures/lecture8/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { hover, motion, stagger, Variants } from "motion/react";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
     height: 100vh;
@@ -7,6 +8,17 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+`;
+
+const BiggerBox = styled.div`
+    width: 600px;
+    height: 600px;
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
@@ -24,15 +36,21 @@ const boxVariants: Variants = {
 };
 
 function App() {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
         <Wrapper>
-            <Box
-                drag
-                variants={boxVariants}
-                whileHover="hover"
-                whileTap="click"
-                whileDrag="drag"
-            ></Box>
+            <BiggerBox ref={biggerBoxRef}>
+                <Box
+                    drag
+                    dragSnapToOrigin
+                    dragElastic={0.5}
+                    dragConstraints={biggerBoxRef}
+                    variants={boxVariants}
+                    whileHover="hover"
+                    whileTap="click"
+                    whileDrag="drag"
+                ></Box>
+            </BiggerBox>
         </Wrapper>
     );
 }
