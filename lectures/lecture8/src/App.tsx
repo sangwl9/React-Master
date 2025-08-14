360331b8b62ba9c1f51f6c532c566c06db945492
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-14 15:23:40 +0900

    8.5 Gestures part One

--- lectures/lecture8/src/App.tsx
+++ lectures/lecture8/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, stagger, Variants } from "motion/react";
+import { hover, motion, stagger, Variants } from "motion/react";
 
 const Wrapper = styled.div`
     height: 100vh;
@@ -12,56 +12,27 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    background-color: rgba(255, 255, 255, 0.2);
+    background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-    width: 70px;
-    height: 70px;
-    border-radius: 35px;
-    place-self: center;
-    background-color: white;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants: Variants = {
-    start: { opacity: 0, scale: 0.5 },
-    end: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-            type: "spring",
-            duration: 2,
-            bounce: 0.5,
-            delayChildren: stagger(0.5, { startDelay: 0.1, from: "first" }),
-        },
-    },
-};
-
-const circleVariants: Variants = {
-    start: { opacity: 0, y: 10 },
-    end: {
-        opacity: 1,
-        y: 0,
-        // transition: {
-        //     delay: 0.5,
-        // },
-    },
+    hover: { scale: 1.5, rotateZ: 90 },
+    click: { scale: 1, borderRadius: "100px" },
+    drag: { backgroundColor: "rgb(46, 204, 113)" },
 };
 
 function App() {
     return (
         <Wrapper>
-            <Box variants={boxVariants} initial="start" animate="end">
-                <Circle variants={circleVariants} />
-                <Circle variants={circleVariants} />
-                <Circle variants={circleVariants} />
-                <Circle variants={circleVariants} />
-            </Box>
+            <Box
+                drag
+                variants={boxVariants}
+                whileHover="hover"
+                whileTap="click"
+                whileDrag="drag"
+            ></Box>
         </Wrapper>
     );
 }
