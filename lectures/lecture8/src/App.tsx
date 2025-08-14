ca4d7993e5a0ddc9429b5d0f6b73baf8fcb1b392
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-14 14:44:00 +0900

    8.3 Variants part One

--- lectures/lecture8/src/App.tsx
+++ lectures/lecture8/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "motion/react";
+import { motion, Variants } from "motion/react";
 
 const Wrapper = styled.div`
     height: 100vh;
@@ -17,14 +17,19 @@ const Box = styled(motion.div)`
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars: Variants = {
+    start: { scale: 0 },
+    end: {
+        scale: 1,
+        rotateZ: 360,
+        transition: { type: "spring", bounce: 0.5 },
+    },
+};
+
 function App() {
     return (
         <Wrapper>
-            <Box
-                transition={{ type: "spring", bounce: 0.5 }}
-                initial={{ scale: 0 }}
-                animate={{ scale: 1, rotateZ: 360 }}
-            />
+            <Box variants={myVars} initial="start" animate="end" />
         </Wrapper>
     );
 }
