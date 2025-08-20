967effb1aaa16ab54022ffa20a2f78ea1a92f118
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-20 17:05:54 +0900

    4.7 useOutletContext

--- lectures/lecture4/src/screens/users/User.tsx
+++ lectures/lecture4/src/screens/users/User.tsx
@@ -1,8 +1,9 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useParams, useOutletContext } from "react-router-dom";
 import { users } from "../../db";
 import { Link } from "react-router-dom";
 
 function User() {
+    console.log(useOutletContext());
     const { userId } = useParams();
     return (
         <div>
@@ -11,7 +12,11 @@ function User() {
             </h1>
             <hr />
             <Link to="followers">See followers</Link>
-            <Outlet />
+            <Outlet
+                context={{
+                    nameOfMyUser: users[Number(userId) - 1].name,
+                }}
+            />
         </div>
     );
 }
