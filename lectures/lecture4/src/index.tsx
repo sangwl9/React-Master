7553dd707ab30e72a1524966668538313cdd9559
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-20 16:37:05 +0900

    4.2 createBrowserRouter

--- lectures/lecture4/src/index.tsx
+++ lectures/lecture4/src/index.tsx
@@ -5,9 +5,10 @@
  */
 
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
 );
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
