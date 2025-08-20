2a229e0c740c42b6cc9357800e79a1be2e4190e5
sangwl92 <115968631+sangwl92@users.noreply.github.com>
2025-08-20 16:58:27 +0900

    4.6 Outlet

--- lectures/lecture4/src/screens/users/User.tsx
+++ lectures/lecture4/src/screens/users/User.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import { users } from "../../db";
+import { Link } from "react-router-dom";
 
 function User() {
     const { userId } = useParams();
     return (
-        <h1>
-            User with id {userId} is named: {users[Number(userId) - 1].name}
-        </h1>
+        <div>
+            <h1>
+                User with id {userId} is named: {users[Number(userId) - 1].name}
+            </h1>
+            <hr />
+            <Link to="followers">See followers</Link>
+            <Outlet />
+        </div>
     );
 }
 export default User;
